f9819c3d7ec828cabaccbf30295c65c0b895509c
Forest Anderson <forestkzanderson@gmail.com>
2024-05-27 20:27:13 -0400

    fix: test

--- cli/build.rs
+++ cli/build.rs
@@ -2,6 +2,7 @@ use anyhow::Result;
 use vergen::{vergen, Config};
 
 fn main() -> Result<()> {
+	// test
 	let mut config = Config::default();
 	*config.git_mut().sha_kind_mut() = vergen::ShaKind::Both;
 	vergen(config)
