69cdf277d75d16e04ffa009306a85b09e2a384a6
Forest Anderson <forestkzanderson@gmail.com>
2024-05-29 10:38:53 -0400

    feat: simplify

--- cli/build.rs
+++ cli/build.rs
@@ -1,8 +1,2 @@
-use anyhow::Result;
-use vergen::{vergen, Config};
-
-fn main() -> Result<()> {
-	let mut config = Config::default();
-	*config.git_mut().sha_kind_mut() = vergen::ShaKind::Both;
-	vergen(config)
+fn main() {
 }
